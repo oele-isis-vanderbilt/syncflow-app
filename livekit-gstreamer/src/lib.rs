a7d4730196148b370274a2d0dbfb9bc9f0618968
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2026-02-15 19:15:21 -0600

    Support multi device single container pipeline

--- livekit-gstreamer/src/lib.rs
+++ livekit-gstreamer/src/lib.rs
@@ -3,11 +3,16 @@ pub mod lk_participant;
 pub mod media_device;
 pub mod media_stream;
 pub mod utils;
+// Only enable this for windows for now
+#[cfg(target_os = "windows")]
+pub mod single_container_pipeline;
 
 pub use devices::*;
 pub use lk_participant::*;
 pub use media_device::*;
 pub use media_stream::*;
+#[cfg(target_os = "windows")]
+pub use single_container_pipeline::*;
 
 pub fn initialize_gstreamer() {
     gstreamer::init().expect("Failed to initialize GStreamer");
